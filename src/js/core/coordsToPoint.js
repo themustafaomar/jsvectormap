e4d74d05549bfb3f306ee86b867f88d539d9a2e4
themustafaomar <themustafaomar@gmail.com>
2023-01-09 17:54:05 +0200

    style: imporve variable declaration

--- src/js/core/coordsToPoint.js
+++ src/js/core/coordsToPoint.js
@@ -2,26 +2,21 @@ import Map from '../map'
 import Proj from '../projection'
 
 export default function coordsToPoint(lat, lng) {
-  let point,
-    proj = Map.maps[this.params.map].projection,
-    centralMeridian = proj.centralMeridian,
-    inset,
-    bbox
+  const projection = Map.maps[this.params.map].projection
+  let { x, y } = Proj[projection.type](lat, lng, projection.centralMeridian)
+  let inset = this.getInsetForPoint(x, y)
 
-  point = Proj[proj.type](lat, lng, centralMeridian)
-  inset = this.getInsetForPoint(point.x, point.y)
-
-  if (inset) {
-    bbox = inset.bbox
-
-    point.x = (point.x - bbox[0].x) / (bbox[1].x - bbox[0].x) * inset.width * this.scale
-    point.y = (point.y - bbox[0].y) / (bbox[1].y - bbox[0].y) * inset.height * this.scale
-
-    return {
-      x: point.x + this.transX * this.scale + inset.left * this.scale,
-      y: point.y + this.transY * this.scale + inset.top * this.scale
-    }
+  if (!inset) {
+    return false
   }
 
-  return false
+  let bbox = inset.bbox
+
+  x = (x - bbox[0].x) / (bbox[1].x - bbox[0].x) * inset.width * this.scale
+  y = (y - bbox[0].y) / (bbox[1].y - bbox[0].y) * inset.height * this.scale
+
+  return {
+    x: x + this.transX * this.scale + inset.left * this.scale,
+    y: y + this.transY * this.scale + inset.top * this.scale
+  }
 }
