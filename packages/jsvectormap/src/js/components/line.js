7f331be1508996ea1702dc310863fbfd18054a1a
Mustafa Omar <themustafaomar@gmail.com>
2025-05-03 04:26:35 +0300

    revert: revert old lineStyle config to maintain compatibility (#194)

--- packages/jsvectormap/src/js/components/line.js
+++ packages/jsvectormap/src/js/components/line.js
@@ -6,7 +6,7 @@ class Line extends BaseComponent {
   constructor(options, style) {
     super()
     this._options = options
-    this._style = style
+    this._style = { initial: style }
     this._draw()
   }
 
@@ -19,7 +19,7 @@ class Line extends BaseComponent {
   }
 
   _draw() {
-    const { index, group, map, animate } = this._options
+    const { index, group, map } = this._options
     const config = {
       d: this._getDAttribute(),
       fill: 'none',
@@ -28,23 +28,18 @@ class Line extends BaseComponent {
 
     this.shape = map.canvas.createPath(config, this._style, group)
     this.shape.addClass(LINE_CLASS)
-
-    if (animate) {
-      this.shape.setStyle({ animation: true })
-    }
   }
 
   _getDAttribute() {
-    const { x1, y1, x2, y2 } = this._options
-    return `M${x1},${y1}${this._getQCommand(x1, y1, x2, y2)}${x2},${y2}`
+    const { x1, y1, x2, y2, curvature } = this._options
+    return `M${x1},${y1}${this._getQCommand(x1, y1, x2, y2, curvature)}${x2},${y2}`
   }
 
-  _getQCommand(x1, y1, x2, y2) {
-    if (!this._options.curvature) {
+  _getQCommand(x1, y1, x2, y2, curvature) {
+    if (!curvature) {
       return ' '
     }
 
-    const curvature = this._options.curvature || 0.6
     const curveX = (x1 + x2) / 2 + curvature * (y2 - y1)
     const curveY = (y1 + y2) / 2 - curvature * (x2 - x1)
 
